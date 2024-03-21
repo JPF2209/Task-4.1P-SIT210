16293421757788e4513475d944442c10491f2e8b
JPF2209 <105408682+JPF2209@users.noreply.github.com>
2024-03-21 18:38:43 +1100

    Update blinker.py

--- blinker.py
+++ blinker.py
@@ -1,7 +1,9 @@
 import RPi.GPIO as GPIO
+#BOARD didn't work so I used BCM instead and I setwarnings to false
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18,GPIO.OUT)
+#Since while 1 didn't work, I set i to 1 and when it's done, I broke the tey and while loop statements
 i = 1
 import time
 try:
